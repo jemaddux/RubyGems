3e1f690c777fcd7eab979cd9b1e51a56c9f0fbb2
John Maddux <jemaddux@gmail.com>
2015-09-17 20:18:10 -0400

    lib

---
+++ lib/nfl_stats.rb
@@ -0,0 +1,24 @@
+require "nfl_stats/version"
+
+module NflStats
+  class Game
+  end
+
+  class Player
+  end
+
+  class Play
+  end
+
+  class Drive
+  end
+
+  class Schedule
+  end
+
+  class Season
+  end
+
+  class Team
+  end
+end
